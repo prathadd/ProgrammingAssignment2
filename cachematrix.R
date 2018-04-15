3848f31837998e43e6c0fc58c1417d80fb5d8e24
Pratha Jhawar <38035905+prathadd@users.noreply.github.com>
2018-04-15 12:52:18 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Programming Assignment 2
+##The funtion takes a vector and dimensions of a matrix to convert into a special matrix within it 
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x, nrow, ncol) {
+  i <- NULL
+  set <- function(y, nrow,ncol) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() {matrix(x,nrow,ncol)}
+  setinverse <- function(solve) i <<- solve
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## Now a list from the above funtion is passed through the following function which will return 
+## the inverse of the special matrix got created in the makeCacheMatrix() funtion  
+## The function also checks if the special matrix is singular and its inverse is NaN. In such case,  
+## the function returns a typed message that the matrix is a "Singular Matrix"
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  dm <- det(data)
+  if (dm==0)
+  {
+    message("Singural Matrix")
+    return()
+  }
+  i <- solve(data)
+  x$setinverse(i)
+  i
 }
